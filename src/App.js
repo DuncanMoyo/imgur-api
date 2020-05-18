0eefb0620503004f553a7bbd278204a7a17f35a4
Duncan Moyo <duncanfmoyo@gmail.com>
2020-05-18 21:53:18 +0200

    titles showing on screen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-import React from 'react';
-import Imgur from './containers/Imgur/Imgur';
+import React, { Component } from "react";
+import Image from '../src/components/Image/Image';
 
-function App() {
-  return (
-    <div className="App">
-      <Imgur />
-      <Imgur />
-      <Imgur />
-    </div>
-  );
+const url = "https://api.imgur.com/3/gallery/hot/viral/1?showViral=true";
+const clientId = "130410711";
+
+class App extends Component {
+
+  state = {
+    images: []
+  }
+
+  componentDidMount() {
+    this.imgurData();
+  }
+
+  imgurData = async () => {
+    const apiResponse = await fetch(url, {
+      headers: {
+        "Content-Type": "Application/json",
+        Authorization: `Client-ID ${clientId}`,
+      },
+    }).then((response) => response.json());
+    console.log(apiResponse);
+    this.setState({images: apiResponse.data})
+  };
+
+  render() {
+
+    const images = this.state.images.map(image => {
+      return <Image title={image.title} key={image.id} />
+    })
+
+    return (
+      <div className="App">
+        {images}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -1,32 +1,16 @@
-import React, { Component } from "react";
-import "./Image.css";
+import React from 'react';
+import './Image.css';
 
-const url = "https://api.imgur.com/3/gallery/hot/viral/1?showViral=true";
-const clientId = "130410711";
-
-class Image extends Component {
-
-  componentDidMount() {
-    this.doStuff()
-  }
-
-  doStuff = async () => {
-    const apiResponse = await fetch(url, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Client-ID ${clientId} `,
-      },
-    }).then((response) => response.json());
-    console.log(apiResponse);
-  };
-
-  render() {
-    return (
-      <div className="Image">
-        <p>Image</p>
+function Image(props) {
+  return (
+    <div>
+      <div className="row">
+        <div className="column">
+          <p>{props.title}</p>      
+        </div>
       </div>
-    );
-  }
+    </div>
+  )
 }
 
 export default Image;
